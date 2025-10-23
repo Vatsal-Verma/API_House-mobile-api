aef7a8a2c147a81475cb133cd95ff83e75da375f
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-10-24 01:14:37 +0530

    Add CrossOrigin annotation to MobileController

--- src/main/java/com/apihouse_mobiles/apihouse_mobiles/MobileController.java
+++ src/main/java/com/apihouse_mobiles/apihouse_mobiles/MobileController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class MobileController {
     
     @Autowired
